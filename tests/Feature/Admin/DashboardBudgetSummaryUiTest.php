5da13d58c520e0cdfd22b310a7e977540fa0d95a
akmalhidayah <akmalhidayat826@gmail.com>
2026-08-13 12:24:34 +0800

    13/08/2026

--- tests/Feature/Admin/DashboardBudgetSummaryUiTest.php
+++ tests/Feature/Admin/DashboardBudgetSummaryUiTest.php
@@ -17,6 +17,7 @@ class DashboardBudgetSummaryUiTest extends TestCase
         $response = $this->actingAs($admin)->get(route('admin.dashboard'));
 
         $response->assertOk();
+        $response->assertSee('dashboard-header sticky top-[52px] z-10', false);
         $response->assertSeeTextInOrder([
             'GENERAL BIAYA JASA',
             'Total Prognosa Biaya',
